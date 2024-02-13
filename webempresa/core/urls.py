e39ebb0663a86eadcfbe80ac3dbd3fbcbb121362
Juan Luis Rastrollo Guerrero <juanlurasgue06@gmail.com>
2024-02-13 20:02:08 +0100

    Added templates

--- webempresa/core/urls.py
+++ webempresa/core/urls.py
@@ -3,12 +3,11 @@ from django.urls import path
 from . import views as core_views
 
 urlpatterns = [
-    path('', core_views.home),
-    path('about', core_views.about),
-    path('services', core_views.services),
-    path('store', core_views.store),
-    path('contact', core_views.contact),
-    path('blog', core_views.blog),
-    path('sample', core_views.sample),
-
+    path('', core_views.home, name="home"),
+    path('about', core_views.about, name="about"),
+    path('services', core_views.services, name="services"),
+    path('store', core_views.store, name="store"),
+    path('contact', core_views.contact, name="contact"),
+    path('blog', core_views.blog, name="blog"),
+    path('sample', core_views.sample, name="sample"),
 ]
--- webempresa/core/views.py
+++ webempresa/core/views.py
@@ -1,24 +1,23 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hola")
+    return render(request, "core/index.html")
 
 def about(request):
-    return HttpResponse("Hola")
+    return render(request, "core/about.html")
 
 def services(request):
-    return HttpResponse("Hola")
+    return render(request, "core/services.html")
 
 def store(request):
-    return HttpResponse("Hola")
+    return render(request, "core/store.html")
 
 def contact(request):
-    return HttpResponse("Hola")
+    return render(request, "core/contact.html")
 
 def blog(request):
-    return HttpResponse("Hola")
+    return render(request, "core/blog.html")
 
 def sample(request):
-    return HttpResponse("Hola")
+    return render(request, "core/sample.html")

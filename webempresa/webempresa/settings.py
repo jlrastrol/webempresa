be74c5c40ffedfd0e50a415511f3a7ea7147e7c5
Juan Luis Rastrollo Guerrero <juanlurasgue06@gmail.com>
2024-02-14 17:54:39 +0100

    Created new social app

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'core',
-    'service.apps.ServiceConfig'
+    'service.apps.ServiceConfig',
+    'social.apps.SocialConfig'
 ]
 
 MIDDLEWARE = [
@@ -65,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social.processors.ctx_dict'
             ],
         },
     },
